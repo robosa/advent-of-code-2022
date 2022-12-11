835a56a2455ba96258faf24a95be6009cce775ba
Romain Bosa <romain.bosa@gmail.com>
2022-12-11 18:31:11 +0100

    day 11 parsing "improvement"

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,4 +1,4 @@
-use std::cmp::Reverse;
+use std::{cmp::Reverse, str::FromStr};
 
 use super::get_input;
 
@@ -17,42 +17,35 @@ struct Monkey {
     count: usize,
 }
 
+fn parse_items(str: &str) -> Option<Vec<u64>> {
+    str.strip_prefix("  Starting items: ")
+        .and_then(|s| s.split(", ").map(|item| item.parse().ok()).collect())
+}
+
+fn parse_op(str: &str) -> Option<Op> {
+    str.strip_prefix("  Operation: new = old ").and_then(|s| {
+        s.split_once(' ').and_then(|op| match op {
+            ("*", "old") => Some(Op::Square),
+            ("*", n) => n.parse().ok().map(Op::Mul),
+            ("+", n) => n.parse().ok().map(Op::Add),
+            _ => None,
+        })
+    })
+}
+
+fn parse_any<T: FromStr>(str: &str) -> Option<T> {
+    str.rsplit_once(' ').and_then(|(_, val)| val.parse().ok())
+}
+
 impl Monkey {
     pub fn new(str: &str) -> Self {
-        let input = str.split('\n').collect::<Vec<_>>();
-        let items = input[1]
-            .strip_prefix("  Starting items: ")
-            .expect("invalid input")
-            .split(", ")
-            .filter_map(|s| s.parse().ok())
-            .collect();
-        let op = match input[2]
-            .strip_prefix("  Operation: new = old ")
-            .and_then(|s| s.split_once(' '))
-        {
-            Some(("*", "old")) => Op::Square,
-            Some(("*", n)) => Op::Mul(n.parse().expect("invalid input")),
-            Some(("+", n)) => Op::Add(n.parse().expect("invalid input")),
-            _ => panic!("invalid input"),
-        };
-        let divisor = input[3]
-            .strip_prefix("  Test: divisible by ")
-            .and_then(|s| s.parse().ok())
-            .expect("invalid input");
-        let idx_true = input[4]
-            .strip_prefix("    If true: throw to monkey ")
-            .and_then(|s| s.parse().ok())
-            .expect("invalid input");
-        let idx_false = input[5]
-            .strip_prefix("    If false: throw to monkey ")
-            .and_then(|s| s.parse().ok())
-            .expect("invalid input");
+        let mut iter = str.split('\n').skip(1);
         Self {
-            items,
-            op,
-            divisor,
-            idx_true,
-            idx_false,
+            items: iter.next().and_then(parse_items).expect("invalid items"),
+            op: iter.next().and_then(parse_op).expect("invalid op"),
+            divisor: iter.next().and_then(parse_any).expect("invalid divisor"),
+            idx_true: iter.next().and_then(parse_any).expect("invalid true idx"),
+            idx_false: iter.next().and_then(parse_any).expect("invalid false idx"),
             count: 0,
         }
     }
@@ -64,15 +57,13 @@ impl Monkey {
                 Op::Square => item * item,
                 Op::Mul(n) => item * n,
                 Op::Add(n) => item + n,
-            };
+            } % max_worry;
             if step == 1 {
                 item /= 3;
             }
-            item %= max_worry;
-            if item % self.divisor == 0 {
-                Some((item, self.idx_true))
-            } else {
-                Some((item, self.idx_false))
+            match item % self.divisor {
+                0 => Some((item, self.idx_true)),
+                _ => Some((item, self.idx_false)),
             }
         } else {
             None
